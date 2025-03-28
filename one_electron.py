f24496f89996d1776fc7be4f474bf8e7e42ea832
Luca Frediani <luca.frediani@gmail.com>
2025-03-28 16:01:08 +0100

    Fix nr of iterations

--- one_electron.py
+++ one_electron.py
@@ -27,7 +27,8 @@ def gs_D_1e(spinorb1, potential, mra, prec, thr, derivative, charge):
     old_energy = 0
     delta_e = 1
     idx = 0
-    while ((error_norm > thr or delta_e > prec/10) and idx < 100):
+#    while ((error_norm > thr or delta_e > prec/10) and idx < 100):
+    while (idx < 30):
         hd_psi = orb.apply_dirac_hamiltonian(spinorb1, prec, der = derivative)
         v_psi = orb.apply_potential(-1.0, potential, spinorb1, prec)
         add_psi = hd_psi + v_psi
@@ -91,7 +92,8 @@ def gs_D2_1e(spinorb1, potential, mra, prec, thr, derivative, charge):
     c2 = light_speed * light_speed
     old_energy = 0
     idx = 0
-    while ((error_norm > thr or delta_e > prec/10) and idx < 100):
+#    while ((error_norm > thr or delta_e > prec/10) and idx < 100):
+    while(idx < 30)
         v_psi = orb.apply_potential(-1.0, potential, spinorb1, prec) 
         vv_psi = orb.apply_potential(-0.5/c2, potential, v_psi, prec*c2)
         beta_v_psi = v_psi.beta2()
